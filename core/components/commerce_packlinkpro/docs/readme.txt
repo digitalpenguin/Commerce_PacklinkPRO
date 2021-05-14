cba5c35fdaa50ba5d288fa62fd3cb414e558b4b3
digitalpenguin <murray@digitalpenguin.hk>
2021-05-14 13:38:14 +0800

    First commit

---
+++ core/components/commerce_packlinkpro/docs/readme.txt
@@ -0,0 +1,5 @@
+PacklinkPRO for Commerce
+------------------------
+
+A short description of what PacklinkPRO does or how to set it up.
+
---
+++ core/components/commerce_packlinkpro/lexicon/en/default.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+$_lang['commerce_packlinkpro'] = 'PacklinkPRO';
+$_lang['commerce_packlinkpro.description'] = 'A short description of what PacklinkPRO does.';
